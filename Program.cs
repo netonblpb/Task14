d4ebc6059c93f3c4950b355d37be0dde47ce52bf
netonblpb <skullbog@gmail.com>
2022-02-04 11:44:56 +0300

    Task14 complete

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Task14: Найти третью цифру числа или сообщить, что её нет
+
+Random rnd = new Random();
+int number = rnd.Next(10, 200);
+Console.WriteLine("Берём число " + number);
+
+string numberCheck = Convert.ToString(number);
+
+if (numberCheck.Length > 2) Console.WriteLine("Третья цифра: " + numberCheck[2]);
+else Console.WriteLine("В числе " + number + " меньше трёх цифр");
